1a25dcadef18cc3a5db0b5b6f69045c9cea7e4de
Valennq12 <valenlovecrafts@gmail.com>
2026-05-20 11:06:01 +0200

    Eliminar carpeta .history con archivos sensibles

--- .history/routes/api_20260314132403.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
-Route::get('/books', [BookController::class, 'index']);
-Route::post('/books', [BookController::class, 'store']);
-Route::get('/books/{id}', [BookController::class, 'show']);
--- .history/routes/api_20260519213106.php
+++
@@ -1,5 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
