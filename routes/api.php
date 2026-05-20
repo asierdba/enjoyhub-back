8a9769ea92eb699d068e02ee03e88d79d2bb5058
asierdba <190217673+asierdba@users.noreply.github.com>
2026-05-20 14:15:25 +0200

    añadido crud de libros para prueba de conexión front-end

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\BookImportController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);  
+Route::post('/books', [BookController::class, 'store']);
 
 Route::post('/books/import/random-genre', [BookImportController::class, 'importRandomByGenre']);
